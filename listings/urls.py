e0f94a259cbd9973a07722b674e718f4ab3cbece
pashaetesam <pauletesam@comcast.net>
2020-02-21 07:23:53 -0800

    7 ...........

--- listings/urls.py
+++ listings/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('',views.index,name='listings'),
     path('<int lisitng_id>',views.listing,name='listing'),
+    path('search',views.search,name='search'),
 ]
